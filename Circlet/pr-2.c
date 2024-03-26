a7b7b382c318d07ab3eaa5861f0f164d9c20345d
RJSakariya <amitdervadiya@gmail.com>
2024-03-26 14:38:44 +0530

    Circlet

---
+++ Circlet/pr-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){
+    int k=11;
+    for (int i = 1; i <= 5; i++){
+        for (int j = 1; j <= i; j++){
+            printf("%d ",k);
+            k++;
+        }
+        printf("\n");
+    }
+}
