944d895ce8c6e3bbf6aaae5381c384282a2a6a60
RJSakariya <amitdervadiya@gmail.com>
2024-03-28 14:48:41 +0530

    PR

---
+++ PR/pr-7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = i; j <= 5; j++){
+            printf(" ");
+        }
+        if (i == 1 || i == 5)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                printf("* ");
+            }
+        }
+        else
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (j == 1||j == i)
+                {
+                    printf("* ");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+        }
+        printf("\n");
+    }
+}
