a5c10eab03adc564f88265bea2280ff8a6b81ef6
RJSakariya <amitdervadiya@gmail.com>
2024-04-09 15:24:51 +0530

    UDF 2

---
+++ UDF 2/pr 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fact(int f) {
+    if(f <= 1){
+        return f;
+    }else{
+        return f *= fact(f-1);
+    }
+}
+
+int main() {
+    int fac;
+    printf("Enter fact size: ");
+    scanf("%d", &fac);
+    printf("%d",fact(fac));
+}
+ 
