2f186a9db3d04f0715111fefb449e3aac35210a1
RJSakariya <amitdervadiya@gmail.com>
2024-04-10 15:16:01 +0530

    Gathering

--- Gathering/pr 3.c
+++ Gathering/pr 3.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int fact(int f) {
-    if(f <= 1){
+    if(f == 1){
         return f;
+    }
+    else if(f <= 0){
+        return 0;
     }else{
         return f *= fact(f-1);
     }
@@ -12,6 +15,10 @@ int main() {
     int fac;
     printf("Enter fact size : ");
     scanf("%d", &fac);
-    printf("%d\n",fact(fac));
+    if(fact(fac)==0){
+        printf("Invalid Number");
+    }else{
+        printf("%d\n",fact(fac));
+    }
 }
  
